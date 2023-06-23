7bc52e79d7a8a4a8402b7b3d50645a99fb5ebf2c
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-06-23 09:50:44 -0500

    :sparkles: (step-3-connect-api) Add auth service and its integration on doLogin method

--- src/app/modules/auth/components/login-form/login-form.component.ts
+++ src/app/modules/auth/components/login-form/login-form.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faPen, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { RequestStatus } from '@models/request-status.model';
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-login-form',
@@ -17,18 +19,30 @@ export class LoginFormComponent {
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
-  status: string = 'init';
+  status: RequestStatus = 'init';
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+        private authService: AuthService
   ) { }
 
   doLogin() {
     if (this.form.valid) {
       this.status = 'loading';
       const { email, password } = this.form.getRawValue();
-      // TODO
+      // TODO: Usar el servicio de Auth
+             this.authService.login(email, password).subscribe({
+                next: () => {
+                    this.status = "success";
+                    this.router.navigate(['/app']);
+                },
+                error: () => {
+                    this.status = "failed";
+                }
+            });
+
+
     } else {
       this.form.markAllAsTouched();
     }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+
+    apiUrl = environment.API_URL;
+
+    constructor(private http: HttpClient) { }
+
+    login(email: string, password: string) {
+        return this.http.post(`${this.apiUrl}/api/v1/auth/login`, {
+            email,
+            password
+        })
+    }
+}
